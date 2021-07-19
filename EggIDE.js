ebf1727857e07cba3660d5a6c7147f14310e1e72
com.vincent <51314855+Vincemcking12@users.noreply.github.com>
2021-07-19 15:33:03 +0200

    Updated the back-end and the IDE front
    Updated the back-end and the IDE front. I also removed all redundant code and comments. It still runs good

--- EggIDE.js
+++ EggIDE.js
@@ -2,6 +2,8 @@
 
 app.LoadScript( "lexer.js"  );
 isDebug = false ;
+
+
 //Called when application is started.
 function OnStart()
 {
@@ -11,25 +13,18 @@ function OnStart()
 	lay = app.CreateLayout( "linear", "VCenter,FillXY" );	
   
       createDlg();
-/*  var str = run(`
-  do(define(x, 4),
-   define(setx, fun(val, +(val,5))),
-   setx(50),
-   print(setx(50)),
-` );*/
    
 	//Create a text label and add it to layout.
-   code = app.CreateTextEdit("", 1,0.9 ,  "Multiline" );
+   code = app.CreateCodeEdit(1,1);
+   code.SetTextSize(10);
+   code.SetNavigationMethod( "Yoyo" );
+   code.SetLanguage(".js");
    lay.AddChild(code);
-	
-	 btn = app.CreateButton("compile" , 1,0.1 );
-	 btn.SetOnTouch(cmpl);
-	 
-	 lay.AddChild(btn);
-	   
 	   
 	//Add layout to app.	
 	app.AddLayout( lay );
+	
+		create_menu();
 }
 
 function OnBack()
@@ -37,9 +32,8 @@ function OnBack()
 //  app.ShowPopup( "Called"  );
 	 if ( isDebug) {
 	     isDebug = false ;
-	     dlg.Dismiss();
-	     txt.Clear();
-	     y = 0.02 ;
+	     
+	  
 	 }
 	 
 }
@@ -52,40 +46,34 @@ function OnBack()
 	        
 	            dlg.Show();
 	            var res = run(code.GetText());
-	             isDebug = true ;
-	             
-	             if ( BUFFER.length > 0 )
-	            {
-	                //alert(BUFFER);
-	                
-	                for ( var i = 0 ; i < BUFFER.length ; i++)
-	                {
-	                    //txt.Log(BUFFER[i]+"\n", "green" );
-	                    txt.DrawText(BUFFER[i] ,  x, y );
-	                    y+= 0.02 ;
-	                    
-	                }
-	                
-	                txt.DrawText("===== Execution success!! ======" , x ,y )
-	                
-	                BUFFER = [] ;
-	                BUFFER_INDEX = BUFFER.length ;
-	            }
-            	 
-            //	 app.Debug(">> "+res );
+	            isDebug = true ;
+	          
+	           topScope.print("===== Execution success!! ======" , x ,y );
               
             	 return res ;
             	 
-            	 }catch(e){ alert(e); } 
+            	 }catch(e){ 
+            	  
+            	   topScope.print(e);
+            	   topScope.print("===== Execution aborted!! ======" , x ,y );
+            	 } 
          }
             	 
   }
   
   
-  
+  function dlg_exit()
+{
+	 dlg.Dismiss();
+	    txt.Clear();
+	     y = 0.02 ;
+	 OnBack();
+}
+
   function createDlg()
 {
     dlg = app.CreateDialog( "running" );
+    dlg.SetOnBack( dlg_exit );
 
     layDlg = app.CreateLayout( "linear", "VCenter,FillXY" );
     layDlg.SetSize( 1, 1 );
@@ -96,6 +84,65 @@ function OnBack()
  //   dlg.Show();
 }
 
+app.LoadPlugin( "UIExtras" );
+
+function create_menu()
+{ 
+ uix = app.CreateUIExtras();
+ 
+ layFam = app.CreateLayout( "Linear", "FillXY, Bottom, Right, TouchThrough" );
+ fam = uix.CreateFAMenu( "[fa-cogs]", "Up,LabelsLeft" );
+ fam.SetMargins( 0.02, 0.01, 0.02, 0.01 );
+ fam.SetLabelBackColor( "#FFFFFF" );
+ fam.SetLabelTextColor( "#646464" );
+ fam.SetOnOpened( fam_OnOpened );
+ fam.SetOnClosed( fam_OnClosed );
+ layFam.AddChild( fam );
+ 
+ fabReply = uix.CreateFAButton( "[fa-code]", "Mini" );
+ fabReply.SetButtonColors( "#db4437", "#c33d32" );
+ fabReply.SetOnTouch( cmpl );
+ fabReply.SetLabel( "Run" );
+ fam.AddFAButton( fabReply );
+ 
+ fabReplyAll = uix.CreateFAButton( "[fa-github]", "Mini" );
+ fabReplyAll.SetButtonColors( "#db4437", "#c33d32" );
+ fabReplyAll.SetOnTouch( fab_OnMailReplyAll );
+ fabReplyAll.SetLabel( "Pull request" );
+ fam.AddFAButton( fabReplyAll );
+ 
+ fabForward = uix.CreateFAButton( "[fa-share]", "Mini" );
+ fabForward.SetButtonColors( "#fbbc05", "#efb306" );
+ fabForward.SetOnTouch( fab_OnMailForward );
+ fabForward.SetLabel( "pull request" );
+ fam.AddFAButton( fabForward );
+ 
+ app.AddLayout( layFam ); 
+}
+function fam_OnOpened()
+{
+ layFam.SetBackColor( "#99FFFFFF" );
+}
+
+function fam_OnClosed()
+{
+ layFam.SetBackColor( "#00FFFFFF" );
+}
+
+function fab_OnMailReply()
+{
+ app.ShowPopup( "Reply" );
+}
+
+function fab_OnMailReplyAll()
+{
+ app.ShowPopup( "Reply All" );
+}
+
+function fab_OnMailForward()
+{
+ app.ShowPopup( "Forward" );
+}
 
   
   
