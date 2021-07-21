3f297fadc5305dfa7e494bea287750c2f83ef2c3
com.vincent <51314855+Vincemcking12@users.noreply.github.com>
2021-07-21 19:51:07 +0200

    Added functionality to the menu buttons
    Added functionality to the menu buttons. I also added object oriented programming features to the language. The code is is not yet documented

---
+++ settings.js
@@ -0,0 +1,43 @@
+
+     {
+   "ide_theme" : "light" ,
+   "float_menu_down_color" :  "#99FFFFFF",
+   "float_menu_up_color" :  "#00FFFFFF",
+   "float_menu_back_color" :  "#FFFFFF" ,
+   "float_menu_text_color" :  "#646464" ,
+   "float_menu_item_back_color" :  "#db4437",
+   "float_menu_item_text_color" : "#c33d32" ,
+   "float_menu_item_altenate_back_color" : "#fbbc05", 
+   "float_menu_item_altenate_text_color" : "#efb306"  ,
+   "ide_font_size":10,
+   "ide_syntax" :  ".js" ,
+   "ide_comment_literal" : "//.*" ,
+   "ide_std_lib": [
+      "math.egg"
+     ],
+    "ide_project_source_path" : "/sdcard/emulated/0/" ,
+    "ide_std_lib_path" : "Std/", 
+   "ide_is_modified" : false ,
+   "compiler_success" :  "===== Execution success!! ======", 
+   "compiler_fail" :  "===== Execution aborted!! ======",
+   "terminal_back_color" : "#000000",
+   "file_index" : 0,
+   "file_select" : "Unsaved" ,
+   "file_list" : [ 
+         "Unsaved"
+   ],
+   
+   "drawer_scroll_back_color": "#ffffff",
+   "drawer_user_text_color" : "#aaaaaa",
+   "drawer_github_repo_text_color" : "#bbbbbb",
+   "drawer_user_text" : "[un-assigned]",
+   "drawer_github_repo_text" : "[github-repo]",
+   "drawer_back_image" : "Img/gridbg.png",
+   "drawer_icon" : "Img/eggicon.png",
+   "drawer_separator_color" :  "#cccccc",
+   "drawer_app_title_text" : "EggIDE",
+   "drawer_app_title_text_color" : "#555555" 
+   
+}
+
+  
